35196d62ee62333f7d8f97c2731e57577c48f3cd
joelborel <joeltchokouassi8@gmail.com>
2024-10-23 16:26:10 +0200

    mon premier depot sur git

--- file_manager.py
+++ file_manager.py
@@ -6,7 +6,7 @@ class FileManager:
        
         logger.add("file_manager.log", format="{time} {level} {message}", level="INFO")
     
-   
+   # lire dans le fichier
     def read_file(self):
         try:
             with open(self.file_path, 'r') as file:
@@ -16,7 +16,7 @@ class FileManager:
         except FileNotFoundError:
             logger.error(f"Le fichier {self.file_path} n'existe pas.")
 
-
+    # ecrire dans le fichier
     def write_to_file(self, data):
         try:
             with open(self.file_path, 'a') as file:
@@ -24,27 +24,7 @@ class FileManager:
                 logger.info(f"Données ajoutées au fichier {self.file_path} : {data}")
         except Exception as e:
             logger.error(f"Erreur lors de l'écriture dans le fichier {self.file_path}: {e}")
-
-    def count_lines(self):
-        try:
-            with open(self.file_path, 'r') as file:
-                lines = file.readlines()
-                logger.info(f"Nombre de lignes dans {self.file_path} : {len(lines)}")
-                return len(lines)
-        except FileNotFoundError:
-            logger.error(f"Le fichier {self.file_path} n'existe pas.")
-            return 0
-
-    def search_keyword(self, keyword):
-        try:
-            with open(self.file_path, 'r') as file:
-                lines = file.readlines()
-                matches = []
-                for num, line in enumerate(lines, 1):
-                    if keyword in line:
-                        matches.append((num, line.strip()))
-                        logger.info(f"Mot-clé '{keyword}' trouvé à la ligne {num}.")
-                return matches
-        except FileNotFoundError:
-            logger.error(f"Le fichier {self.file_path} n'existe pas.")
-            return []
+           
+           
+        return []
+        
--- main.py
+++ main.py
@@ -11,9 +11,7 @@ def afficher_menu():
     print("\nQue voulez-vous faire ?")
     print("1. Lire le fichier")
     print("2. Écrire dans le fichier")
-    print("3. Rechercher un mot-clé dans le fichier")
-    print("4. compter le nombre de ligne dans le fichier")
-    print("5. Quitter")
+    print("3. Quitter")
 
 # Boucle principale pour interagir avec l'utilisateur
 while True:
@@ -30,22 +28,12 @@ while True:
         data = input("Entrez le texte à ajouter au fichier: ")
         file_manager.write_to_file(data)
         print(f"Le texte '{data}' a été ajouté au fichier.")
-
+  
     elif choix == '3':
-        # Rechercher un mot-clé
-        keyword = input("Entrez le mot-clé à rechercher dans le fichier: ")
-        print(f"Recherche du mot-clé '{keyword}':")
-        file_manager.search_keyword(keyword)
-
-    elif choix == '4':
-        nombre_lignes = file_manager.count_lines() 
-        print(f" le fichier a {nombre_lignes} lignes") 
-
-    elif choix == '5':
         # Quitter le programme
         print("Au revoir!")
         break
 
 
     else:
-        print("Choix invalide. Veuillez entrer 1, 2, 3 ou 4.")
+        print("Choix invalide. Veuillez entrer 1, 2 ou 3 ")
